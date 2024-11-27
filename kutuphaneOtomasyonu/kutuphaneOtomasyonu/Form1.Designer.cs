bda95a9366b28d439e6bce2cdd0ebd45c13eec44
Yakup DELIBAS <yakupdelibas767@gmail.com>
2024-11-27 21:12:57 +0300

    ikinci commit alt sekmeler

--- kutuphaneOtomasyonu/kutuphaneOtomasyonu/Form1.Designer.cs
+++ kutuphaneOtomasyonu/kutuphaneOtomasyonu/Form1.Designer.cs
@@ -29,13 +29,141 @@ namespace kutuphaneOtomasyonu
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.kISILERToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.kITAPISLEMLERIToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.kITAPLARToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.kITAPISLEMLERIToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.kITAPISLEMLERIToolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.kITAPLARToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.kITAPVERToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tESLİMETToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.kITAPALToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tESLIMALToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.kISILERToolStripMenuItem,
+            this.kITAPISLEMLERIToolStripMenuItem,
+            this.kITAPVERToolStripMenuItem,
+            this.kITAPALToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(898, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // kISILERToolStripMenuItem
+            // 
+            this.kISILERToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.kITAPISLEMLERIToolStripMenuItem1,
+            this.kITAPLARToolStripMenuItem});
+            this.kISILERToolStripMenuItem.Image = global::kutuphaneOtomasyonu.Properties.Resources.Contacts_icon__svg;
+            this.kISILERToolStripMenuItem.Name = "kISILERToolStripMenuItem";
+            this.kISILERToolStripMenuItem.Size = new System.Drawing.Size(140, 24);
+            this.kISILERToolStripMenuItem.Text = "KISI ISLEMLERI";
+            // 
+            // kITAPISLEMLERIToolStripMenuItem1
+            // 
+            this.kITAPISLEMLERIToolStripMenuItem1.Name = "kITAPISLEMLERIToolStripMenuItem1";
+            this.kITAPISLEMLERIToolStripMenuItem1.Size = new System.Drawing.Size(202, 26);
+            this.kITAPISLEMLERIToolStripMenuItem1.Text = "KITAP ISLEMLERI";
+            // 
+            // kITAPLARToolStripMenuItem
+            // 
+            this.kITAPLARToolStripMenuItem.Name = "kITAPLARToolStripMenuItem";
+            this.kITAPLARToolStripMenuItem.Size = new System.Drawing.Size(202, 26);
+            this.kITAPLARToolStripMenuItem.Text = "KITAPLAR";
+            // 
+            // kITAPISLEMLERIToolStripMenuItem
+            // 
+            this.kITAPISLEMLERIToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.kITAPISLEMLERIToolStripMenuItem2,
+            this.kITAPLARToolStripMenuItem1});
+            this.kITAPISLEMLERIToolStripMenuItem.Image = global::kutuphaneOtomasyonu.Properties.Resources.book;
+            this.kITAPISLEMLERIToolStripMenuItem.Name = "kITAPISLEMLERIToolStripMenuItem";
+            this.kITAPISLEMLERIToolStripMenuItem.Size = new System.Drawing.Size(153, 24);
+            this.kITAPISLEMLERIToolStripMenuItem.Text = "KITAP ISLEMLERI";
+            // 
+            // kITAPISLEMLERIToolStripMenuItem2
+            // 
+            this.kITAPISLEMLERIToolStripMenuItem2.Name = "kITAPISLEMLERIToolStripMenuItem2";
+            this.kITAPISLEMLERIToolStripMenuItem2.Size = new System.Drawing.Size(202, 26);
+            this.kITAPISLEMLERIToolStripMenuItem2.Text = "KITAP ISLEMLERI";
+            // 
+            // kITAPLARToolStripMenuItem1
+            // 
+            this.kITAPLARToolStripMenuItem1.Name = "kITAPLARToolStripMenuItem1";
+            this.kITAPLARToolStripMenuItem1.Size = new System.Drawing.Size(202, 26);
+            this.kITAPLARToolStripMenuItem1.Text = "KITAPLAR";
+            // 
+            // kITAPVERToolStripMenuItem
+            // 
+            this.kITAPVERToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tESLİMETToolStripMenuItem});
+            this.kITAPVERToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("kITAPVERToolStripMenuItem.Image")));
+            this.kITAPVERToolStripMenuItem.Name = "kITAPVERToolStripMenuItem";
+            this.kITAPVERToolStripMenuItem.Size = new System.Drawing.Size(111, 24);
+            this.kITAPVERToolStripMenuItem.Text = "KITAP VER";
+            // 
+            // tESLİMETToolStripMenuItem
+            // 
+            this.tESLİMETToolStripMenuItem.Name = "tESLİMETToolStripMenuItem";
+            this.tESLİMETToolStripMenuItem.Size = new System.Drawing.Size(160, 26);
+            this.tESLİMETToolStripMenuItem.Text = "TESLİM ET";
+            // 
+            // kITAPALToolStripMenuItem
+            // 
+            this.kITAPALToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tESLIMALToolStripMenuItem});
+            this.kITAPALToolStripMenuItem.Image = global::kutuphaneOtomasyonu.Properties.Resources.transparent_recycling;
+            this.kITAPALToolStripMenuItem.Name = "kITAPALToolStripMenuItem";
+            this.kITAPALToolStripMenuItem.Size = new System.Drawing.Size(102, 24);
+            this.kITAPALToolStripMenuItem.Text = "KITAP AL";
+            // 
+            // tESLIMALToolStripMenuItem
+            // 
+            this.tESLIMALToolStripMenuItem.Name = "tESLIMALToolStripMenuItem";
+            this.tESLIMALToolStripMenuItem.Size = new System.Drawing.Size(161, 26);
+            this.tESLIMALToolStripMenuItem.Text = "TESLIM AL";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(898, 478);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "KUTUPHANE OTOMASYONU";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem kISILERToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem kITAPISLEMLERIToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem kITAPLARToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem kITAPISLEMLERIToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem kITAPISLEMLERIToolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem kITAPLARToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem kITAPVERToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tESLİMETToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem kITAPALToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tESLIMALToolStripMenuItem;
     }
 }
 
--- kutuphaneOtomasyonu/kutuphaneOtomasyonu/formKisiIslrmlri.cs
+++ kutuphaneOtomasyonu/kutuphaneOtomasyonu/formKisiIslrmlri.cs
@@ -3,18 +3,17 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace kutuphaneOtomasyonu
 {
-    public partial class Form1 : Form
+    public partial class formKisiIslrmlri : Form
     {
-        public Form1()
+        public formKisiIslrmlri()
         {
             InitializeComponent();
         }
+        
     }
 }
